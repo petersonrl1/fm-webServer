d87d7f844e2672eebbd6f8414172c68a4e70c2da
Ross Peterson <petersonrl1@gmail.com>
2025-04-06 17:26:05 -0400

    adding db

--- index-ws.js
+++ index-ws.js
@@ -1,41 +1,48 @@
-const express = require("express");
-const server = require("http").createServer();
+const express = require('express');
+const server = require('http').createServer();
 const app = express();
-const PORT = 3000;
 
-app.get("/", function (req, res) {
-  res.sendFile("index.html", { root: __dirname });
+app.get('/', function(req, res) {
+  res.sendFile('index.html', {root: __dirname});
 });
 
-server.on("request", app);
+server.on('request', app);
 
-server.listen(PORT, function () {
-  console.log("Listening on " + PORT);
+process.on('SIGINT', () => {
+  server.close(() => {
+    shutdownDB(); 
+  });
 });
+
+server.listen(3000, function () { console.log('Listening on 3000'); });
 
 /** Websocket **/
-const WebSocketServer = require("ws").Server;
+const WebSocketServer = require('ws').Server;
 
 const wss = new WebSocketServer({ server: server });
 
-wss.on("connection", function connection(ws) {
+wss.on('connection', function connection(ws) {
   const numClients = wss.clients.size;
 
-  console.log("clients connected: ", numClients);
+  console.log('clients connected: ', numClients);
+
+  // Log number of visitors at current moment
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES (${numClients}, datetime('now'))`);
 
   wss.broadcast(`Current visitors: ${numClients}`);
 
   if (ws.readyState === ws.OPEN) {
-    ws.send("welcome!");
+    ws.send('welcome!');
   }
 
-  ws.on("close", function close() {
+  ws.on('close', function close() {
     wss.broadcast(`Current visitors: ${wss.clients.size}`);
-    console.log("A client has disconnected");
+    console.log('A client has disconnected');
   });
 
-  ws.on("error", function error() {
-    //
+  ws.on('error', function error() {
+ 
   });
 });
 
@@ -45,9 +52,38 @@ wss.on("connection", function connection(ws) {
  * @void
  */
 wss.broadcast = function broadcast(data) {
-  console.log("Broadcasting: ", data);
+  console.log('Broadcasting: ', data);
   wss.clients.forEach(function each(client) {
     client.send(data);
   });
 };
 /** End Websocket **/
+
+/** Database stuff **/
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database(':memory:');
+
+// .seralize ensures DB is set up before any queries
+db.serialize(() => {
+    db.run(`CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+      )`);
+});
+
+function getCounts() {
+    db.each("SELECT * FROM visitors", (err, row) => {
+      console.log(row);
+    });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log('shutting down DB');
+  db.close();
+}
+
+
+
+/** End Database stuff **/
